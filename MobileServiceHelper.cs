d87e1f49b2483edcf1436b58cbba6ccf5b09627b
Bin Gao <bingao@g.harvard.edu>
2015-02-24 23:25:36 -0500

    Initial check-in.

---
+++ MobileServiceHelper.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+
+namespace TaskApp
+{
+
+	/// <summary>
+	/// Dynamically instancated single instance of the Mobile Service client
+	/// </summary>
+	public class MobileServiceHelper
+	{
+
+		private static MobileServiceHelper _instance;
+
+		const string applicationURL = @"https://dotnet2.azure-mobile.net/";
+		const string applicationKey = @"vJvQmMAfYHXtPOXyVgtiPLKiaoyNVl83";
+
+		private readonly MobileServiceClient _client;
+
+		/// <summary>
+		/// Prevents a default instance of the <see cref="MobileServiceHelper"/> class from being created.
+		/// </summary>
+		private MobileServiceHelper()
+		{
+			CurrentPlatform.Init();
+
+			// Only needed when using offline features.
+			// SQLitePCL.CurrentPlatform.Init();
+
+			// Initialize the Mobile Service client with your URL and key
+			_client = new MobileServiceClient(applicationURL, applicationKey);
+
+		}
+
+		/// <summary>
+		/// The _sync root used for save multi-threaded creation/access to the singleton
+		/// </summary>
+		private static volatile object _syncRoot = new object();
+
+		/// <summary>
+		/// Gets the service client.
+		/// </summary>
+		/// <value>The service client.</value>
+		public MobileServiceClient ServiceClient { get { return _client; } }
+
+		/// <summary>
+		/// Gets the default service, instaciating it if it does not exist
+		/// Uses the double check pattern
+		/// </summary>
+		/// <value>The default service.</value>
+		public static MobileServiceHelper DefaultService
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (_syncRoot)
+					{
+						if (_instance == null)
+						{
+							_instance = new MobileServiceHelper();
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+	}
+}
---
+++ QuestionItem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TriviaManage
+{
+	public class QuestionItem
+	{
+		private string _id;
+
+		[JsonProperty(PropertyName = "id")]
+		public string Id
+		{
+			get { return _id; }
+			set
+			{
+				_id = value;
+			}
+		}
+
+		private string _text;
+
+		[JsonProperty(PropertyName = "questionText")]
+		public string questionText
+		{
+			get { return _text; }
+			set
+			{
+				_text = value;
+			}
+		}
+
+		private string _one;
+
+		[JsonProperty(PropertyName = "answerOne")]
+		public string answerOne
+		{
+			get { return _one; }
+			set
+			{
+				_one = value;
+			}
+		}
+
+		private string _two;
+
+		[JsonProperty(PropertyName = "answerTwo")]
+		public string answerTwo
+		{
+			get { return _two; }
+			set
+			{
+				_two = value;
+			}
+		}
+
+		private string _three;
+
+		[JsonProperty(PropertyName = "answerThree")]
+		public string answerThree
+		{
+			get { return _three; }
+			set
+			{
+				_three = value;
+			}
+		}
+
+		private string _four;
+
+		[JsonProperty(PropertyName = "answerFour")]
+		public string answerFour
+		{
+			get { return _four; }
+			set
+			{
+				_four = value;
+			}
+		}
+
+		private string _identifier;
+
+		[JsonProperty(PropertyName = "identifier")]
+		public string identifier
+		{
+			get { return _identifier; }
+			set
+			{
+				_identifier = value;
+			}
+		}
+	}
+}
